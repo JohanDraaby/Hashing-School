f35ec36ea747dfff611aa888c36d2d0396c6fda8
JohanDraaby <Jolle.draaby@gmail.com>
2020-10-27 08:38:13 +0100

    First Commit

---
+++ Hashing/HashData.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace Hashing
+{
+    class HashData
+    {
+        public static byte[] ComputeHashSha512(byte[] toBeHashed)
+        {
+            using (var sha512 = SHA512.Create())
+            {
+                return sha512.ComputeHash(toBeHashed);
+            }
+        }
+    }
+}
---
+++ Hashing/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace Hashing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = HMAC.GenerateKey();
+
+
+            Console.WriteLine("==== Chose ====");
+            Console.WriteLine("1: - Hash");
+            Console.WriteLine();
+            Console.WriteLine("2: - HMAC");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("HASHING");
+                    Console.WriteLine("Insert Text To Be Hashed");
+                    input = Console.ReadLine();
+                    Console.WriteLine();
+
+                    var sha512HashedMessage = HashData.ComputeHashSha512(Encoding.UTF8.GetBytes(input));
+
+                    Console.WriteLine("Input: - " + input);
+                    Console.WriteLine();
+                    Console.WriteLine("Hashed: - " + Convert.ToBase64String(sha512HashedMessage));
+
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("HMAC");
+                    Console.WriteLine("Insert Text To HMAC");
+                    input = Console.ReadLine();
+                    Console.WriteLine();
+
+                    var hmacSha512Message = HMAC.ComputeHmacsha512(Encoding.UTF8.GetBytes(input), key);
+
+                    Console.WriteLine("Input: - " + input);
+                    Console.WriteLine();
+                    Console.WriteLine("HMAC: - " + Convert.ToBase64String(hmacSha512Message));
+
+
+                    break;
+            }
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
